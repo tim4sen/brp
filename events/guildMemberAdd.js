a52cde62427d3539a644c4dc0742af2e853e91b3
tim4sen <tim4sen@gmail.com>
2024-12-19 23:43:34 +0000

    Create guildMemberAdd.js

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,32 @@
+module.exports = {
+  execute(member, client) {
+    console.log("Event fired for member:", member.user.tag);
+
+    const roleId = "1319439177359556671";
+    const role = member.guild.roles.cache.get(roleId);
+
+    if (role) {
+      member.roles
+        .add(role)
+        .then(() => {
+          console.log(`Assigned role to ${member.user.tag}`);
+        })
+        .catch((error) => {
+          console.error("Error assigning role:", error);
+        });
+    } else {
+      console.error("Role not found");
+    }
+
+    const joinLogChannelId = "1317969567011835948";
+    const channel = client.channels.cache.get(joinLogChannelId);
+
+    if (channel) {
+      channel.send(
+        `<:bpwave:1319437396873449583> Welcome, <@${member.user.id}> has joined the server!`
+      );
+    } else {
+      console.error("Join log channel not found!");
+    }
+  },
+};
