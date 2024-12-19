237988d5df7c7f63be0f38650ca3336d0e909dc4
tim4sen <tim4sen@gmail.com>
2024-12-19 23:43:55 +0000

    Create guildeMemberRemove.js

---
+++ events/guildeMemberRemove.js
@@ -0,0 +1,13 @@
+module.exports = {
+  execute(member, client) {
+    console.log('Event fired for member:', member.user.tag);
+    const leaveLogChannelId = '1319426298380025906';
+    const channel = client.channels.cache.get(leaveLogChannelId);
+
+    if (channel) {
+      channel.send(`<:bpwave:1319437396873449583> Sad to see you go, <@${member.user.id}> has left the server.`);
+    } else {
+      console.error('Leave log channel not found!');
+    }
+  },
+};
